17d10750702da0a1299409d4ac7bc14f4f86baa2
Hojin Kim <hojiny9923@gmail.com>
2021-01-14 21:29:40 +0900

    add Timetext

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public float remainTime;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainTime = 120f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainTime > 0)
+        {
+            remainTime -= Time.deltaTime;
+            UpdateText();
+        }
+    }
+
+    void UpdateText()
+    {
+        int min, sec;
+        min = (int)(remainTime / 60f);
+        sec = (int)(remainTime % 60f);
+
+        timeText.text = min + ":" + sec.ToString("D2");
+    }
+}
